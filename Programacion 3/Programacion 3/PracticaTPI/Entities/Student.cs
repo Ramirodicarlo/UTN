6c4f8af05bc72c22231bcc7b1ba0bb24f13d81b0
Ramiro Di Carlo <ramirodicarlo2@gmail.com>
2023-10-05 16:27:44 -0300

    varios

---
+++ Programacion 3/Programacion 3/PracticaTPI/Entities/Student.cs
@@ -0,0 +1,13 @@
+﻿using PracticaTPI.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaTPI.Entities
+{
+    public class Student: IStudent
+    {
+    }
+}
